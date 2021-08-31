8cdb04e157117a1eebf24446f3242b92cefa2393
EvenW <yuhang0229@gmail.com>
2021-08-31 13:05:58 +1000

    use package dotenv for appid

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 
 const router = express.Router();
 
+const APPID = process.env.APPID;
+
 router.get('/:city', (req,res) => {
   //res.send('weather');
   const { city } = req.params;
 
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d02e4b47c5bf2bd3c9e43fe05aebaee7`)
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APPID}`)
     .then(response => {
       res.send(response.data);
     })
