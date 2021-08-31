4db6022d2a9763819a29ffe7b5b8860a6e5f7365
EvenW <yuhang0229@gmail.com>
2021-08-31 13:38:42 +1000

    add winston, morgan, helmet package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 const routes = require('./routes');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const logger = require('./utils/logger')
 
 
 const app = express();
+app.use(helmet());
 
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}else {
+  app.use(morgan('common'));
+}
 app.use(routes);
 
 const PORT = process.env.PORT || 9000;
@@ -13,5 +22,5 @@ app.listen(PORT, (err) =>{
   if(err) {
     console.error("Something wrong!");
   }
-  console.log(`server working...... on port ${PORT}`);
+  logger.info(`server working...... on port ${PORT}`);
 })
