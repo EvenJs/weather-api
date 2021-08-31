2a6b3664bfa10b7fe6f9fbf8739cefc6cba3a25d
EvenW <yuhang0229@gmail.com>
2021-08-31 18:44:07 +1000

    add errorHandler

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -3,17 +3,18 @@ const express = require('express');
 const weather = require('../models/Weather');
 const responseFormatter = require('../utils/responseFormatter');
 
-const router = express.Router();
-const APPID = process.env.APPID;
 
-router.get('/:city', (req,res) => {
+const router = express.Router();
+// const APPID = process.env.APPID;
+
+router.get('/:city', (req, res, next) => {
   //res.send('weather');
   const { city } = req.params;
   const weatherType = req.query.weatherType;
   weather
     .getData(city)
     .then(response => responseFormatter(res, 200, null, response))
-    .catch(err => console.log(err));
+    .catch(err => next(err));
 });
 
 module.exports = router;
